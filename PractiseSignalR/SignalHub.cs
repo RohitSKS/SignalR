1c7371c55b3e0067caaac46379a80326b7b93362
RohitSKS <vinodsingh44786@gmail.com>
2019-08-23 10:21:06 +0530

    SignalR first commit

---
+++ PractiseSignalR/SignalHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace PractiseSignalR
+{
+    public class SignalHub:Hub
+    {
+        public void Announce(string user, string message)
+        {
+            Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+
+}
---
+++ PractiseSignalR/wwwroot/js/Chat.js
@@ -0,0 +1,33 @@
+﻿$.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/aspnet-signalr/1.1.4/signalr.js');
+const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/signalhub")
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
+
+connection.on("ReceiveMessage", (user, message) => {
+    const encodedMsg = user + " says " + message;
+    const li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+});
+
+document.getElementById("sendButton").addEventListener("click", event => {
+    const user = document.getElementById("userInput").value;
+    const message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", user, message).catch(err => console.error(err.toString()));
+    event.preventDefault();
+});
+
+async function start() {
+    try {
+        await connection.start();
+        console.log("connected");
+    } catch (err) {
+        console.log(err);
+        setTimeout(() => start(), 5000);
+    }
+};
+
+connection.onclose(async () => {
+    await start();
+});
